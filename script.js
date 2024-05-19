cb66922ba686052e50a38e7c15f81bc3ee13a59d
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-20 00:22:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById('signup-btn').addEventListener('click', function() {
   window.location.href = 'signup.html';
 });
+
+document.getElementById('network-btn').addEventListener('click', function() {
+  window.location.href = 'network.html';
+});
+
+document.getElementById('jobs-btn').addEventListener('click', function() {
+  window.location.href = 'jobs.html';
+});
+
+document.getElementById('resources-btn').addEventListener('click', function() {
+  window.location.href = 'resources.html';
+});
